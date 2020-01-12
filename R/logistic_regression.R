9550bbbe7ed91458523530af0e611e4ab0d50a5f
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2020-01-12 22:46:13 +0000

    put sigmoid inside the object

--- R/logistic_regression.R
+++ R/logistic_regression.R
@@ -1,7 +1,6 @@
 run <- function(lr) UseMethod("run")
 grad_ascent <- function(lr) UseMethod("grad_ascent")
 newton_method <- function(lr) UseMethod("newton_method")
-sigmoid <- function(lr) UseMethod("sigmoid")
 
 #' Logistic Regression S3 Object
 #'
@@ -30,6 +29,8 @@ sigmoid <- function(lr) UseMethod("sigmoid")
 #' @examples
 logistic_regression <- function(X, y, cost="MLE", method="BFGS", sigmab=1.0, niter=100,
                                 alpha=0.1, gamma=0.001){
+  # since R is stupid, add logistic as a field
+  sigmoid <- function(x) 1.0 / (1.0 + exp(-x))
   start <- matrix(0, nrow=ncol(X))
   # Define cost functions
   mle_cost <- function(beta) sum(log(1 + exp((1 - 2*y) * (X %*% beta))))
@@ -39,7 +40,8 @@ logistic_regression <- function(X, y, cost="MLE", method="BFGS", sigmab=1.0, nit
   else if (cost == "MAP") costfunc <- map_cost
   # S3 object creation
   lr <- list(start=start, X=X, y=y, cost=cost, method=method, sigmab=sigmab, niter=niter,
-             alpha=alpha, gamma=gamma, costfunc=costfunc, beta=NULL, hessian=NULL)
+             alpha=alpha, gamma=gamma, costfunc=costfunc, beta=NULL, hessian=NULL,
+             sigmoid=sigmoid)
   print(method)
   print(method=="BFGS")
   # Run optimization
@@ -77,18 +79,15 @@ print.logistic_regression <- function(x, ...){
     cat("Solution:             ", x$beta, "\n")
   }
 
-
-sigmoid <- function(x) 1.0 / (1.0 + exp(-x))
-
 grad_ascent<- function(lr){
   beta <- lr$start
   if (lr$cost=="MLE"){
     for (i in 1:lr$niter) {
-      beta <- beta + lr$gamma * t(lr$X) %*% (lr$y - sigmoid(lr$X %*% beta))
+      beta <- beta + lr$gamma * t(lr$X) %*% (lr$y - lr$sigmoid(lr$X %*% beta))
     }
   } else if (lr$cost=="MAP"){
     for (i in 1:lr$niter) {
-      beta <- beta + lr$gamma*(lr$sigmab^2*t(lr$X) %*% (lr$y - sigmoid(lr$X %*% beta)) - beta)
+      beta <- beta + lr$gamma*(lr$sigmab^2*t(lr$X) %*% (lr$y - lr$sigmoid(lr$X %*% beta)) - beta)
     }
   }
   return(beta)
@@ -100,17 +99,17 @@ newton_method <- function(lr){
   beta <- lr$start
   if (lr$cost=="MLE"){
     for (i in 1:lr$niter){
-      D_k <- diag(drop(sigmoid(lr$X%*%beta)*(1 - sigmoid(lr$X%*%beta))))
-      d_k <- solve(t(lr$X)%*%D_k %*% lr$X, lr$alpha*t(lr$X) %*% (lr$y - sigmoid(lr$X %*% beta)))
+      D_k <- diag(drop(lr$sigmoid(lr$X%*%beta)*(1 - lr$sigmoid(lr$X%*%beta))))
+      d_k <- solve(t(lr$X)%*%D_k %*% lr$X, lr$alpha*t(lr$X) %*% (lr$y - lr$sigmoid(lr$X %*% beta)))
       beta <- beta + d_k
     }
   } else if (lr$cost=="MAP"){
     n <- ncol(lr$X)
     for (i in 1:lr$niter){
-      D_k <- diag(drop(sigmoid(lr$X%*%beta)*(1 - sigmoid(lr$X%*%beta))))
+      D_k <- diag(drop(lr$sigmoid(lr$X%*%beta)*(1 - lr$sigmoid(lr$X%*%beta))))
       d_k <- solve(
         lr$sigmab^2*t(lr$X)%*%D_k%*%lr$X + diag(n),
-        lr$alpha*(lr$sigmab^2*t(lr$X)%*%(lr$y - sigmoid(lr$X %*% beta)) - beta)
+        lr$alpha*(lr$sigmab^2*t(lr$X)%*%(lr$y - lr$sigmoid(lr$X %*% beta)) - beta)
       )
       beta <- beta + d_k
     }
